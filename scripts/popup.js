b5f26457de22aa8cd67a8f675654a1d66faf8af5
emmafurlong1 <emmafurlong@blueyonder.co.uk>
2022-09-19 13:33:42 +0100

    popup testing

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,13 +2,13 @@
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var a = document.getElementById("artist");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+a.onclick = function() {
   modal.style.display = "block";
 }
 
